63c0ef949adc633e05a843f3dbbdc4577005af46
Ana Julia S. Sobral <101907723+The-AnJu@users.noreply.github.com>
2023-12-11 19:03:47 -0300

    desafio extra

--- Exercícios/aula1.py
+++ Exercícios/aula1.py
@@ -19,3 +19,7 @@ print('Digite um segundo numero:')
 n2 = input()
 soma = float(n1)+float(n2)
 print('A soma dos dois numeros é: ', soma)
+
+#desafio extra
+
+print('A soma entre {} e {} vale {}'.format(n1, n2, int(soma))) 
