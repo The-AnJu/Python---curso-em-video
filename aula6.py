d73d67e643b3dc0d9c1e11c5a586aa43fa77cd91
Ana Julia S. Sobral <101907723+The-AnJu@users.noreply.github.com>
2023-12-13 18:59:59 -0300

    ex004

---
+++ aula6.py
@@ -0,0 +1,13 @@
+#Desafio 4
+
+a = input('Digite algo: ') #como eu posso mudar o tipo, quando o código estiver rodando? Ou sempre será string?
+print('O tipo primitivo deste valor é:', type(a))
+print('Só tem espaços? ', a.isspace())
+print('É um número? ', a.isnumeric())
+print('É um alfabético? ', a.isalpha())
+print('É alfanumérico? ', a.isalnum())
+print('Está em letras maiusculas? ', a.isupper())
+print('Está em letras minusculas? ', a.islower())
+print('Está capitalizada? ', a.istitle())
+
+
