e61b32c2b250c0b99d94b6806c3b0df413dcf36f
Ana Julia S. Sobral <101907723+The-AnJu@users.noreply.github.com>
2023-12-13 19:26:53 -0300

    ex004 com método

--- aula6.py
+++ aula6.py
@@ -10,4 +10,7 @@ print('Está em letras maiusculas? ', a.isupper())
 print('Está em letras minusculas? ', a.islower())
 print('Está capitalizada? ', a.istitle())
 
+#Desafio 4 com método
 
+a = input('Digite algo:')
+print('O tipo primitivo deste valor é {}. Só há espaços ? {} É numérico? {} É alfabético? {} É alfanumérico? {} Está em letras maiusculas? {} Está em letras minusculas? {} Está capitalizada? {}' .format(type(a), a.isspace(), a.isnumeric(), a.isalpha(), a.isalnum(), a.isupper(), a.islower(), a.istitle()))
