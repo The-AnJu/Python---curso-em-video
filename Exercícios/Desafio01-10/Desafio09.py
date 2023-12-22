196aca900b72336ed76dc5f26747dfd15fa5e3fe
Ana Julia S. Sobral <101907723+The-AnJu@users.noreply.github.com>
2023-12-21 21:02:54 -0300

    desafio09, pensando

---
+++ Exercícios/Desafio01-10/Desafio09.py
@@ -0,0 +1,3 @@
+n = int(input('Digite um número qualquer: '))
+t = (n*1, n*2, n*3, n*4, n*5, n*6, n*7, n*8, n*9, n*10)
+print('Tabuada de {}: 0:nx1=, 1:nx2=, nx3={2}, nx4={3}, nx5={4}, nx6={5}, nx7={6}, nx8={7}, nx9={8}, nx10+{9}' .format(n, t))
