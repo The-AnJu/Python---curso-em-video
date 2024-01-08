0c82324b2b016a2ac857e668820efc475e7191aa
Ana Julia S. Sobral <101907723+The-AnJu@users.noreply.github.com>
2024-01-08 08:31:06 -0300

    implementando medidas

--- Exercícios/Desafio01-10/Desafio08.py
+++ Exercícios/Desafio01-10/Desafio08.py
@@ -1,5 +1,14 @@
 #Desafio08
 
-m = int(input('Valor em metro: '))
-c = float(m*100)
-print('Valor em centimetros: {:.3f} cm' .format(c))
+m = int(input('Distância em metros: '))
+print('Uma distância de {} metros, corresponde a:'.format(m))
+km = m / 1000
+print('{:.3f}km'.format(km))
+hm = m / 100
+print('{:.3f}hm'.format(hm))
+dam = m / 10
+print('{:.3f}dam'.format(dam))
+dm = m * 10
+cm = m * 100
+mm = m *1000
+print('{}dm \n{}cm \n{}mm'.format(dm, cm, mm))
