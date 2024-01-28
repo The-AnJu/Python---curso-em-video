3821da5bd06f30d8b15a7d59a05a472cf205a1c6
Ana Julia S. Sobral <101907723+The-AnJu@users.noreply.github.com>
2024-01-28 14:28:14 -0300

    desafios111213

--- Exercícios/Desafio01-10/Desafio11.py
+++ Exercícios/Desafio01-10/Desafio11.py
@@ -5,4 +5,4 @@ a = float(input('Altura: '))
 ar = l*a
 print('Sua parede tem a dimensão de {} x {} e sua área é de {}m².' .format(l, a, ar))
 li = ar/2
-print('Para pintar sua parede, você precisará de {} l de tinta.' .format(li))
+print('Para pintar sua parede, você precisará de {:.4f}l de tinta.' .format(li))
